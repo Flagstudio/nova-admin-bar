c6b3c1d170b8cf96c0911c46199fa742554b966f
vasishakd <vasishak.d@gmail.com>
2019-05-31 11:55:27 +0500

    getModel method

--- src/AdminBar.php
+++ src/AdminBar.php
@@ -7,10 +7,14 @@ class AdminBar
     private static $model = null;
     private static $resource = '';
 
+    /**
+     * @return string
+     * @throws \Throwable
+     */
     public function generate()
     {
         if (self::$model) {
-            $resourceName = self::$resource ?: getModel(self::$model, false);
+            $resourceName = self::$resource ?: $this->getModel(self::$model);
             $resource = (config('adminbar.resources_namespace') ?: '\\App\\Nova\\') . $resourceName;
             $uriKey = $resource::uriKey();
             $showLink = config('nova.path') . '/resources/' . $uriKey . '/' . self::$model->id;
@@ -20,13 +24,30 @@ class AdminBar
         return view('NovaAdminBar::bar', compact('editLink', 'showLink'))->render();
     }
 
+    /**
+     * @param $model
+     */
     public function setModel($model)
     {
         self::$model = $model;
     }
 
-    public function setResource($resource)
+    /**
+     * @param string $resource
+     */
+    public function setResource(string $resource)
     {
         self::$resource = $resource;
+    }
+
+    /**
+     * @param $object
+     * @return string
+     */
+    function getModel($object): string
+    {
+        $model = explode('\\', get_class($object));
+
+        return $model[count($model) - 1];
     }
 }
